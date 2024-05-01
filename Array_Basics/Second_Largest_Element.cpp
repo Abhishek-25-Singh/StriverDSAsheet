202c33cc6bd78a28bf9761cc3282352fdf9a94fb
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-05-01 15:35:25 +0530

    Optimal Approach
    Best Way To Solve The Second And Largest Element in the Sorted,unique and non - neagtive ELements....
     if (arr1[i] > largest_Element) {
                Second_Largest = largest_Element;
                largest_Element = arr1[i];
                
            } else if (arr1[i]<largest_Element && arr1[i] > Second_Largest) {
                Second_Largest = arr1[i];
            }
    This else if condition is inside a loop iterating over the elements of the array arr1. It's designed to update the Second_Largest variable  if the current element (arr1[i]) is less than the largest_Element but greater than the current Second_Largest element found so far.

--- Array_Basics/Second_Largest_Element.cpp
+++ Array_Basics/Second_Largest_Element.cpp
@@ -53,5 +53,38 @@ int main()
     }
     std::cout << "Second Largest Element is " << Second_Largest_Element << std::endl;
 
+      // ************ Optimal Approach ***********
+
+     
+    int arr1[1000];
+    int n;
+    std::cout << "ENter The Number Of Element in the array:";
+    std::cin >> n;
+    std::cout << "Enter The ELement :";
+    for (int i = 0; i < n; i++) {
+        std::cin >> arr1[i];
+    }
+    int largest_Element = arr1[0];
+    int Second_Largest = -1;//If Negative Element is Present use INT_MIN
+    for (int i = 0; i < n; i++) {
+        if (arr1[i] > largest_Element) {
+            Second_Largest = largest_Element;
+            largest_Element = arr1[i];
+            
+        }
+        /*
+        This else if condition is inside a loop iterating over the 
+        elements of the array arr1. It's designed to update the Second_Largest 
+        variable 
+        if the current element (arr1[i]) is less than the largest_Element 
+        but greater than the current Second_Largest element found so far.
+        */
+        else if (arr1[i]<largest_Element && arr1[i] > Second_Largest) {
+            Second_Largest = arr1[i];
+        }
+    }
+    std::cout << "largest_Element :" << largest_Element << std::endl;
+    std::cout << "Second_Largest :" << Second_Largest << std::endl;
+
     return 0;
 }
